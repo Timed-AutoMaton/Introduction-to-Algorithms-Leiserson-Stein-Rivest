d097de451179479f02de936df27c5b563df29332
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2026-06-26 10:52:31 +0500

    learning

---
+++ C++/BubbleSort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    cout << "Enter the element in array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        for (j = 0; j <=i;)
+    }
+}
